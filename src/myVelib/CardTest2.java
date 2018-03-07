d7c5e7b4b2e219e392182f9d91c5b7c794a7fe3b
Léonard Boussioux <leonard.boussioux@student.ecp.fr>
2018-03-07 21:01:54 +0100

    NewWorld

---
+++ src/myVelib/CardTest2.java
@@ -0,0 +1,14 @@
+package myVelib;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardTest2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
